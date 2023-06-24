479717342ed1d0b5cf0e48081558f8b3ee2aa1a3
Iana <malovayai@gmail.com>
2023-06-24 14:28:31 +0300

    передача параметров в директиву

--- src/app/Directives/style.directive.ts
+++ src/app/Directives/style.directive.ts
@@ -1,9 +1,12 @@
-import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
+  @Input('appStyle') color: string = 'blue';
+  @Input() fontWeight = 'normal';
+  @Input() dStyle: {border?: string, borderRadius?: string} = {};
   constructor(private el: ElementRef, private r: Renderer2) {
    // el.nativeElement.style.color = 'red';
   }
@@ -14,11 +17,18 @@ export class StyleDirective {
 
   //при наведении на параграф, он меняет цвет
   @HostListener('mouseenter', ['$event']) onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+    this.r.setStyle(this.el.nativeElement, 'color', this.color);
     this.r.setStyle(this.el.nativeElement, 'cursor', 'pointer');
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight);
+    this.r.setStyle(this.el.nativeElement, 'border', this.dStyle.border);
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyle.borderRadius);
   }
 
   @HostListener('mouseleave', ['$event']) onLeave() {
     this.r.setStyle(this.el.nativeElement, 'color', null);
+    this.r.setStyle(this.el.nativeElement, 'cursor', null);
+    this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
+    this.r.setStyle(this.el.nativeElement, 'border', null);
+    this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
   }
 }
