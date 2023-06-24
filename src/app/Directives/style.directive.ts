88675b3608b1d839bac5271f1deaae1f7ebb44dd
Iana <malovayai@gmail.com>
2023-06-24 14:37:25 +0300

    изменение цвета с помощью HostBinding

--- src/app/Directives/style.directive.ts
+++ src/app/Directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
@@ -7,6 +7,8 @@ export class StyleDirective {
   @Input('appStyle') color: string = 'blue';
   @Input() fontWeight = 'normal';
   @Input() dStyle: {border?: string, borderRadius?: string} = {};
+
+  @HostBinding('style.color') elColor = '';
   constructor(private el: ElementRef, private r: Renderer2) {
    // el.nativeElement.style.color = 'red';
   }
@@ -17,18 +19,20 @@ export class StyleDirective {
 
   //при наведении на параграф, он меняет цвет
   @HostListener('mouseenter', ['$event']) onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', this.color);
-    this.r.setStyle(this.el.nativeElement, 'cursor', 'pointer');
-    this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight);
-    this.r.setStyle(this.el.nativeElement, 'border', this.dStyle.border);
-    this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyle.borderRadius);
+    this.elColor = this.color;
+    // this.r.setStyle(this.el.nativeElement, 'color', this.color);
+    // this.r.setStyle(this.el.nativeElement, 'cursor', 'pointer');
+    // this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight);
+    // this.r.setStyle(this.el.nativeElement, 'border', this.dStyle.border);
+    // this.r.setStyle(this.el.nativeElement, 'borderRadius', this.dStyle.borderRadius);
   }
 
   @HostListener('mouseleave', ['$event']) onLeave() {
-    this.r.setStyle(this.el.nativeElement, 'color', null);
-    this.r.setStyle(this.el.nativeElement, 'cursor', null);
-    this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
-    this.r.setStyle(this.el.nativeElement, 'border', null);
-    this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
+    this.elColor = '';
+    // this.r.setStyle(this.el.nativeElement, 'color', null);
+    // this.r.setStyle(this.el.nativeElement, 'cursor', null);
+    // this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
+    // this.r.setStyle(this.el.nativeElement, 'border', null);
+    // this.r.setStyle(this.el.nativeElement, 'borderRadius', null);
   }
 }
