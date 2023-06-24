2e1ea4f857cefd223cd724794b43e1a22407e25f
Iana <malovayai@gmail.com>
2023-06-24 13:54:19 +0300

    изменение цвета параграфа при наведении курсора

--- src/app/Directives/style.directive.ts
+++ src/app/Directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appStyle]'
@@ -6,6 +6,19 @@ import {Directive, ElementRef, Renderer2} from "@angular/core";
 export class StyleDirective {
   constructor(private el: ElementRef, private r: Renderer2) {
    // el.nativeElement.style.color = 'red';
+  }
+
+  @HostListener('click', ['$event']) onClick(event: Event) {
+    console.log(event);
+  }
+
+  //при наведении на параграф, он меняет цвет
+  @HostListener('mouseenter', ['$event']) onEnter() {
     this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+    this.r.setStyle(this.el.nativeElement, 'cursor', 'pointer');
+  }
+
+  @HostListener('mouseleave', ['$event']) onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'color', null);
   }
 }
