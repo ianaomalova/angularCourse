a8568679a5ceb4753804cf4682db856963aeeec3
Iana <malovayai@gmail.com>
2023-06-26 20:42:34 +0300

    формы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,53 @@
-import { Component } from '@angular/core';
-import {AppCounterService} from "./services/app-counter.service";
-import {LocalServiceService} from "./services/local-service.service";
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [LocalServiceService]
 })
-export class AppComponent {
-  constructor(private appCounterService: AppCounterService, private localService: LocalServiceService) {
+export class AppComponent implements OnInit {
+  form: FormGroup = new FormGroup<any>({});
+  constructor() {
 
   }
-  getterAppCounter() {
-    return this.appCounterService;
+
+  ngOnInit(): void {
+    this.form = new FormGroup<any>({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      address: new FormGroup({
+        country: new FormControl('ru'),
+        city: new FormControl('', Validators.required)
+      }),
+      skills: new FormArray([]),
+    });
   }
 
-  getterLocalCounter() {
-    return this.localService;
+  submit() {
+    console.log(this.form);
+    const formData = {...this.form.value};
+    console.log(formData)
   }
 
+  setCapital() {
+    const cityMap = {
+      ru: 'Москва',
+      by: 'Минск',
+      ua: 'Киев',
+    }
+
+    const cityKey = this.form.get('address')?.get('country')?.value;
+    // @ts-ignore
+    const city = cityMap[cityKey];
+    this.form.patchValue({
+      address: {city: city}
+    })
+  }
+
+  addSkill() {
+    const control = new FormControl('', Validators.required);
+    // (<FormArray>this.form.get('skills')).push('');
+    // (this.form.get('skills') as FormArray).push('');
+  }
 }
